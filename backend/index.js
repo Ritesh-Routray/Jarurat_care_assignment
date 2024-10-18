54a72ca37b43cd7c0cdd6da611d07ae5619f6972
Ritesh <riteshroutray2005@gmail.com>
2024-10-18 10:17:10 +0530

    3rd commit

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,12 @@ app.use("/api/service",serviceRoutes)
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 app.listen(process.env.PORT || 6060 , () => {
     connectDB();
